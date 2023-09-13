4f8a3e6e3df3dab4a42702c0730ace8caa81b01c
ntitsolutins01 <ntitsolutions01@gmail.com>
2023-09-12 21:36:53 -0300

    Add project files.

---
+++ WebApp/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebApp.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class LoginModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<LoginModel> _logger;
+
+        public LoginModel(SignInManager<IdentityUser> signInManager, 
+            ILogger<LoginModel> logger,
+            UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public LoginInput Login { get; set; }
+        
+
+		public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+		public class LoginInput : IValidatableObject
+		{
+			[RegularExpression(@"^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$", ErrorMessage = "O e-mail informado deve atender um formato padrão válido.")]
+			public string Email { get; set; }
+
+			[DataType(DataType.Password)] public string Password { get; set; }
+
+			[Display(Name = "Remember me?")] public bool RememberMe { get; set; }
+
+			public bool Submitted { get; set; } = false;
+
+			public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+			{
+				var results = new List<ValidationResult>();
+
+				if (Submitted)
+				{
+					if (string.IsNullOrEmpty(Email))
+						results.Add(new ValidationResult("Your email address is required", new[] { "Email" }));
+
+					if (string.IsNullOrEmpty(Password))
+						results.Add(new ValidationResult("Your password is required", new[] { "Password" }));
+				}
+
+				return results;
+			}
+		}
+
+		public async Task OnGetAsync(string returnUrl = null)
+        {
+            if (!string.IsNullOrEmpty(ErrorMessage))
+            {
+                ModelState.AddModelError(string.Empty, ErrorMessage);
+            }
+
+            returnUrl ??= Url.Content("~/");
+
+            // Clear the existing external cookie to ensure a clean login process
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+        
+            if (ModelState.IsValid)
+            {
+                // This doesn't count login failures towards account lockout
+                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                var result = await _signInManager.PasswordSignInAsync(Login.Email, Login.Password, Login.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User logged in.");
+                    return LocalRedirect(returnUrl);
+                }
+                if (result.RequiresTwoFactor)
+                {
+                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Login.RememberMe });
+                }
+                if (result.IsLockedOut)
+                {
+                    _logger.LogWarning("User account locked out.");
+                    return RedirectToPage("./Lockout");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return Page();
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+    }
+}
---
+++ WebApp/wwwroot/js/login.js
@@ -0,0 +1,71 @@
+﻿(function () {
+
+    'use strict';
+
+    // basic
+    $("#form").validate({
+        rules: {
+            "Login.Email": {
+                required: true,
+                email: true
+            },
+            "Login.Password": {
+                required: true,
+                minlength: 8
+            },
+            "Input.Password": {
+                required: true,
+                minlength: 8
+            },
+            "Input.ConfirmPassword": {
+                required: true,
+                minlength: 8,
+                //equalTo: "#Input.Password"
+            },
+            "Input.Email": {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            "Login.Email": {
+                required: "Por favor informe seu usuário.",
+                email: "Formato de e-mail inválido."
+            },
+            "Login.Password": {
+                required: "Por favor informe sua senha.",
+                minlength: jQuery.validator.format("Formato de senha inválido, a senha deve conter no mínimo 8 digitos.")
+            },
+            "Input.Password": {
+                required: "Por favor informe sua senha.",
+                minlength: jQuery.validator.format("Formato de senha inválido, a senha deve conter no mínimo 8 digitos.")
+            },
+            "Input.ConfirmPassword": {
+                required: "Por favor informe sua senha.",
+                minlength: jQuery.validator.format("Formato de senha inválido, a senha deve conter no mínimo 8 digitos."),
+                //equalTo: "As senhas digitadas são diferentes. Por favor, repita a operação."
+            },
+            "Input.Email": {
+                required: "Por favor informe seu e-mail.",
+                email: "Formato de e-mail inválido."
+            }
+        },
+        highlight: function (label) {
+            $(label).closest('.form-group').removeClass('has-success').addClass('has-error');
+        },
+        success: function (label) {
+            $(label).closest('.form-group').removeClass('has-error');
+            label.remove();
+        },
+        errorPlacement: function (error, element) {
+            var placement = element.closest('.input-group');
+            if (!placement.get(0)) {
+                placement = element;
+            }
+            if (error.text() !== '') {
+                placement.after(error);
+            }
+        }
+    });
+
+}).apply(this, [jQuery]);
