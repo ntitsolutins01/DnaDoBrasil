d559da9c1b3294e643d0a66b460e91e5c1e7c49d
Hugo <leao1414@hotmail.com>
2023-11-07 15:07:41 -0300

    chore: implementacao de codigo de frontend para contrato

---
+++ WebApp/ApiClient/ContratoClient.cs
@@ -0,0 +1,62 @@
+﻿using WebApp.Dto;
+using WebApp.Models;
+
+namespace WebApp.ApiClient
+{
+    public partial class DnaApiClient
+    {
+        #region Main Methods
+
+        public Task<long> CreateContrato(ContratoModel.CreateUpdateContratoCommand command)
+        {
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                $"{ResourceConfiguracaoSistema}"));
+            return Post(requestUrl, command);
+        }
+        public Task<long> UpdateContrato(ContratoModel.CreateUpdateContratoCommand command)
+        {
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                $"{ResourceConfiguracaoSistema}"));
+            return Put(requestUrl, command);
+        }
+        public List<ContratoDto> GetContratoAll()
+        {
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                $"{ResourceConfiguracaoSistema}"));
+            return Get<List<ContratoDto>>(requestUrl);
+        }
+
+        public Task<long> DeleteContrato(string id)
+        {
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                $"{ResourceConfiguracaoSistema}/{id}"));
+            return Delete<string>(requestUrl);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public ContratoDto GetContratoById(string id)
+        {
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                $"{ResourceConfiguracaoSistema}/{id}"));
+            return Get<ContratoDto>(requestUrl);
+        }
+        //public List<ContratoDto> GetContratosAll()
+        //{
+        //    var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+        //        $"{ResourceConfiguracaoSistema}/Contratos"));
+        //    return Get<List<ContratoDto>>(requestUrl);
+        //}
+
+        //public Task<bool> ExistUsuarioByIdContrato(string id)
+        //{
+        //    var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+        //        $"{ResourceConfiguracaoSistema}/ExistUsuarioByIdContrato"));
+        //    return PostAsync<bool, string>(requestUrl, id);
+        //}
+
+        #endregion
+    }
+}
---
+++ WebApp/Controllers/ContratoController.cs
@@ -0,0 +1,97 @@
+﻿using Infraero.Relprev.CrossCutting.Enumerators;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApp.Areas.Identity.Models;
+using WebApp.Enumerators;
+using WebApp.Factory;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+	public class ContratoController : BaseController
+    {
+        public IActionResult Index(int? crud, int? notify, string message = null)
+        {
+            SetNotifyMessage(notify, message);
+            SetCrudMessage(crud);
+            var response = ApiClientFactory.Instance.GetContratoAll();
+            return View(new ContratoModel(){Contratos = response});
+        }
+
+        //[ClaimsAuthorize("ConfiguracaoSistema", "Incluir")]
+        public ActionResult Create(int? crud, int? notify, string message = null)
+        {
+            SetNotifyMessage(notify, message);
+            SetCrudMessage(crud);
+
+            return View();
+        }
+
+        //[ClaimsAuthorize("Usuario", "Incluir")]
+        public async Task<ActionResult> Create(IFormCollection collection)
+        {
+            try
+            {
+                var command = new ContratoModel.CreateUpdateContratoCommand
+                {
+
+                    Nome = collection["nome"].ToString(),
+                    Descricao = collection["descricao"].ToString(),
+                    DtIni = Convert.ToDateTime(collection["dtIni"].ToString()),
+                    DtFim = Convert.ToDateTime(collection["dtFim"].ToString())
+                };
+
+                await ApiClientFactory.Instance.CreateContrato(command);
+
+                return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Created });
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        //[ClaimsAuthorize("Contrato", "Alterar")]
+        //public ActionResult Edit(string id)
+        //{
+        //    var obj = ApiClientFactory.Instance.GetContratoById(id);
+
+        //    var model = new ContratoModel() { Contrato = obj };
+
+        //    return View(model);
+        //}
+
+        //[ClaimsAuthorize("Usuario", "Alterar")]
+        public async Task<ActionResult> Edit(string id, IFormCollection collection)
+        {
+                var command = new ContratoModel.CreateUpdateContratoCommand
+                {
+
+                    Id = Convert.ToInt32(id),
+                    Nome = collection["nome"].ToString(),
+                    Descricao = collection["descricao"].ToString(),
+                    DtIni = Convert.ToDateTime(collection["dtIni"].ToString()),
+                    DtFim = Convert.ToDateTime(collection["dtFim"].ToString())
+                };
+
+                await ApiClientFactory.Instance.UpdateUsuario(command);
+
+                return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Updated });
+        }
+
+        //[ClaimsAuthorize("Usuario", "Excluir")]
+        public ActionResult Delete(string id)
+        {
+            try
+            {
+                ApiClientFactory.Instance.DeleteContrato(id);
+                return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Deleted });
+            }
+            catch
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+    }
+}
