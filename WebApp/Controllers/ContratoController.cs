4854faa5a7a1b4e3bdf555acd3c04422edbb1c0d
drkinfo616@gmail.com <drkinfo616@gmail.com>
2023-11-08 18:21:12 -0300

    chore: implementação do front end para inclusão, exclusão e alteração de contrato

--- WebApp/Controllers/ContratoController.cs
+++ WebApp/Controllers/ContratoController.cs
@@ -10,88 +10,81 @@ using WebApp.Models;
 namespace WebApp.Controllers
 {
 	public class ContratoController : BaseController
-    {
-        public IActionResult Index(int? crud, int? notify, string message = null)
-        {
-            SetNotifyMessage(notify, message);
-            SetCrudMessage(crud);
-            var response = ApiClientFactory.Instance.GetContratoAll();
-            return View(new ContratoModel(){Contratos = response});
-        }
+	{
+		public IActionResult Index(int? crud, int? notify, string message = null)
+		{
+			//SetNotifyMessage(notify, message);
+			//SetCrudMessage(crud);
+			//var response = ApiClientFactory.Instance.GetContratoAll();
 
-        //[ClaimsAuthorize("ConfiguracaoSistema", "Incluir")]
-        public ActionResult Create(int? crud, int? notify, string message = null)
-        {
-            SetNotifyMessage(notify, message);
-            SetCrudMessage(crud);
+			//return View(new ContratoModel(){Contratos = response});
+			return View();
+		}
 
-            return View();
-        }
+		//[ClaimsAuthorize("ConfiguracaoSistema", "Incluir")]
+		public ActionResult Create(int? crud, int? notify, string message = null)
+		{
+			SetNotifyMessage(notify, message);
+			SetCrudMessage(crud);
 
-        //[ClaimsAuthorize("Usuario", "Incluir")]
-        public async Task<ActionResult> Create(IFormCollection collection)
-        {
-            try
-            {
-                var command = new ContratoModel.CreateUpdateContratoCommand
-                {
+			return View();
+		}
 
-                    Nome = collection["nome"].ToString(),
-                    Descricao = collection["descricao"].ToString(),
-                    DtIni = Convert.ToDateTime(collection["dtIni"].ToString()),
-                    DtFim = Convert.ToDateTime(collection["dtFim"].ToString())
-                };
+		//[ClaimsAuthorize("Usuario", "Incluir")]
+		[HttpPost]
+		public async Task<ActionResult> Create(IFormCollection collection)
+		{
+			try
+			{
+				var command = new ContratoModel.CreateUpdateContratoCommand
+				{
+					Nome = collection["nome"].ToString().ToUpper(),
+					Descricao = collection["descricao"].ToString(),
+					DtIni = Convert.ToDateTime(collection["dtini"].ToString()),
+					DtFim = Convert.ToDateTime(collection["dtfim"].ToString()),
+					Anexo = collection["anexo"].ToString()
+				};
 
-                await ApiClientFactory.Instance.CreateContrato(command);
+				await ApiClientFactory.Instance.CreateContrato(command);
 
-                return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Created });
-            }
-            catch (Exception e)
-            {
-                return RedirectToAction(nameof(Index));
-            }
-        }
+				return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Created });
+			}
+			catch (Exception e)
+			{
+				return RedirectToAction(nameof(Index));
+			}
+		}
 
-        //[ClaimsAuthorize("Contrato", "Alterar")]
-        //public ActionResult Edit(string id)
-        //{
-        //    var obj = ApiClientFactory.Instance.GetContratoById(id);
+		//[ClaimsAuthorize("Usuario", "Alterar")]
+		public async Task<ActionResult> Edit(string id, IFormCollection collection)
+		{
+			var command = new ContratoModel.CreateUpdateContratoCommand
+			{
+				Id = Convert.ToInt32(id),
+				Nome = collection["nome"].ToString(), 
+				Descricao = collection["descricao"].ToString(),
+				DtIni = Convert.ToDateTime(collection["dtini"].ToString()),
+				DtFim = Convert.ToDateTime(collection["dtfim"].ToString()),
+				Anexo = collection["anexo"].ToString()
+			};
 
-        //    var model = new ContratoModel() { Contrato = obj };
+			await ApiClientFactory.Instance.UpdateUsuario(command);
 
-        //    return View(model);
-        //}
+			return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Updated });
+		}
 
-        //[ClaimsAuthorize("Usuario", "Alterar")]
-        public async Task<ActionResult> Edit(string id, IFormCollection collection)
-        {
-                var command = new ContratoModel.CreateUpdateContratoCommand
-                {
-
-                    Id = Convert.ToInt32(id),
-                    Nome = collection["nome"].ToString(),
-                    Descricao = collection["descricao"].ToString(),
-                    DtIni = Convert.ToDateTime(collection["dtIni"].ToString()),
-                    DtFim = Convert.ToDateTime(collection["dtFim"].ToString())
-                };
-
-                await ApiClientFactory.Instance.UpdateUsuario(command);
-
-                return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Updated });
-        }
-
-        //[ClaimsAuthorize("Usuario", "Excluir")]
-        public ActionResult Delete(string id)
-        {
-            try
-            {
-                ApiClientFactory.Instance.DeleteContrato(id);
-                return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Deleted });
-            }
-            catch
-            {
-                return RedirectToAction(nameof(Index));
-            }
-        }
-    }
+		//[ClaimsAuthorize("Usuario", "Excluir")]
+		public ActionResult Delete(string id)
+		{
+			try
+			{
+				ApiClientFactory.Instance.DeleteContrato(id);
+				return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Deleted });
+			}
+			catch
+			{
+				return RedirectToAction(nameof(Index));
+			}
+		}
+	}
 }
