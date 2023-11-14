ed3c263aac024f47ca3378ec92e09266cd25511f
DRKinfo <126280960+DRKinfo@users.noreply.github.com>
2023-11-14 18:15:35 -0300

    chore: implementação de client para divisão administrativa

---
+++ WebApp/Dto/MunicipioDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp.Dto
+{
+    public class MunicipioDto
+    {
+		public int Id { get; init; }
+		public int Codigo { get; init; }
+		public string? Nome { get; init; }
+		public EstadoDto? Estado { get; init; }
+
+	}
+}
