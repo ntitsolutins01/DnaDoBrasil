ec31f2896daffa6364895161142d4b224afaa9c0
FabioMuniz <ntitsolutions01@gmail.com>
2023-12-04 13:29:57 -0300

    chore: Codigo fomento para front

---
+++ WebApp/Dto/FomentoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp.Dto
+{
+    public class FomentoDto
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public int MunicipioId { get; set; }
+        public int LocalidadeId { get; set; }
+    }
+}
--- WebApp/Models/FomentoModel.cs
+++ WebApp/Models/FomentoModel.cs
@@ -11,6 +11,14 @@ namespace WebApp.Models
         public SelectList ListEstados { get; set; }
         public string CidadeId { get; set; }
         public SelectList ListCidades { get; set; }
-	}
+
+        public class CreateUpdateFomentoCommand
+        {
+            public string Id { get; set; }
+            public string Nome { get; set; }
+            public int MunicipioId { get; set; }
+            public int LocalidadeId { get; set; }
+        }
+    }
 
 }
