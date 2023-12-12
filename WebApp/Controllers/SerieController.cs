c1924473bc41e90984641665dbbebea43408231a
DRKinfo <126280960+DRKinfo@users.noreply.github.com>
2023-12-12 11:58:51 -0300

    gitflow-feature-stash: dev-drk

--- WebApp/Controllers/SerieController.cs
+++ WebApp/Controllers/SerieController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using WebApp.Configuration;
+using WebApp.Dto;
 using WebApp.Enumerators;
 using WebApp.Factory;
 using WebApp.Models;
@@ -8,102 +9,89 @@ using WebApp.Utility;
 
 namespace WebApp.Controllers
 {
-    public class SerieController : BaseController
-    {
+	public class SerieController : BaseController
+	{
+		private readonly IOptions<UrlSettings> _appSettings;
 
-	    private readonly IOptions<UrlSettings> _appSettings;
-
-	    public SerieController(IOptions<UrlSettings> appSettings)
-	    {
-		    _appSettings = appSettings;
-		    ApplicationSettings.WebApiUrl = _appSettings.Value.WebApiBaseUrl;
+		public SerieController(IOptions<UrlSettings> appSettings)
+		{
+			_appSettings = appSettings;
+			ApplicationSettings.WebApiUrl = _appSettings.Value.WebApiBaseUrl;
 		}
 
-	    public IActionResult Index(int? crud, int? notify, string message = null)
-        {
-            SetNotifyMessage(notify, message);
-            SetCrudMessage(crud);
-            //var response = ApiClientFactory.Instance.GetSerieAll();
+		public IActionResult Index(int? crud, int? notify, string message = null)
+		{
+			SetNotifyMessage(notify, message);
+			SetCrudMessage(crud);
+			var response = ApiClientFactory.Instance.GetSerieAll();
 
-            //return View(new SerieModel(){Series = response});
-            return View();
-        }
+			return View(new SerieModel() { Series = response });
+		}
 
-        //[ClaimsAuthorize("ConfiguracaoSistema", "Incluir")]
-        public ActionResult Create(int? crud, int? notify, string message = null)
-        {
-            SetNotifyMessage(notify, message);
-            SetCrudMessage(crud);
+		//[ClaimsAuthorize("ConfiguracaoSistema", "Incluir")]
+		public ActionResult Create(int? crud, int? notify, string message = null)
+		{
+			SetNotifyMessage(notify, message);
+			SetCrudMessage(crud);
 
-            return View();
-        }
+			return View();
+		}
 
-        //[ClaimsAuthorize("Usuario", "Incluir")]
-        [HttpPost]
-        public async Task<ActionResult> Create(IFormCollection collection)
-        {
-            try
-            {
-                var command = new SerieModel.CreateUpdateSerieCommand
-                {
+		//[ClaimsAuthorize("Usuario", "Incluir")]
+		[HttpPost]
+		public async Task<ActionResult> Create(IFormCollection collection)
+		{
+			try
+			{
+				var command = new SerieModel.CreateUpdateSerieCommand
+				{
+					Nome = collection["nome"].ToString()
+				};
 
-                    Nome = collection["nome"].ToString(),
-                    Descricao = collection["descricao"].ToString(),
-                    IdadeInicial = Convert.ToInt32(collection["idadeInicial"]),
-                    IdadeFinal = Convert.ToInt32(collection["idadeFinal"]),
-                    ScoreTotal = Convert.ToInt32(collection["scoreTotal"].ToString())
-    };
+				await ApiClientFactory.Instance.CreateSerie(command);
 
-                await ApiClientFactory.Instance.CreateSerie(command);
+				return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Created });
+			}
+			catch (Exception e)
+			{
+				return RedirectToAction(nameof(Index));
+			}
+		}
 
-                return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Created });
-            }
-            catch (Exception e)
-            {
-                return RedirectToAction(nameof(Index));
-            }
-        }
+		//[ClaimsAuthorize("Usuario", "Alterar")]
+		public async Task<ActionResult> Edit(IFormCollection collection)
+		{
+			var command = new SerieModel.CreateUpdateSerieCommand
+			{
+				Id = Convert.ToInt32(collection["editSerieId"]),
+				Nome = collection["nome"].ToString(),
+				Status = collection["editStatus"].ToString() == "" ? false : true
+			};
 
-        //[ClaimsAuthorize("Serie", "Alterar")]
-        //public ActionResult Edit(string id)
-        //{
-        //    var obj = ApiClientFactory.Instance.GetSerieById(id);
+			await ApiClientFactory.Instance.UpdateSerie(command.Id, command);
 
-        //    var model = new SerieModel() { Serie = obj };
+			return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Updated });
+		}
 
-        //    return View(model);
-        //}
+		//[ClaimsAuthorize("Usuario", "Excluir")]
+		public ActionResult Delete(int id)
+		{
+			try
+			{
+				ApiClientFactory.Instance.DeleteSerie(id);
+				return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Deleted });
+			}
+			catch
+			{
+				return RedirectToAction(nameof(Index));
+			}
+		}
 
-        //[ClaimsAuthorize("Usuario", "Alterar")]
-        public async Task<ActionResult> Edit(string id, IFormCollection collection)
-        {
-                var command = new SerieModel.CreateUpdateSerieCommand
-                {
-                    Id = Convert.ToInt32(id),
-                    Nome = collection["nome"].ToString(),
-                    Descricao = collection["descricao"].ToString(),
-                    IdadeInicial = Convert.ToInt32(collection["idadeInicial"]),
-                    IdadeFinal = Convert.ToInt32(collection["idadeFinal"]),
-                    ScoreTotal = Convert.ToInt32(collection["scoreTotal"].ToString())
-                };
+		public async Task<SerieDto> GetSerieById(int id)
+		{
+			var result = ApiClientFactory.Instance.GetSerieById(id);
 
-                //await ApiClientFactory.Instance.UpdateSerie(command);
-
-                return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Updated });
-        }
-
-        //[ClaimsAuthorize("Usuario", "Excluir")]
-        public ActionResult Delete(string id)
-        {
-            try
-            {
-                //ApiClientFactory.Instance.DeleteSerie(id);
-                return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Deleted });
-            }
-            catch
-            {
-                return RedirectToAction(nameof(Index));
-            }
-        }
-    }
+			return result;
+		}
+	}
 }
--- WebApp/Models/SerieModel.cs
+++ WebApp/Models/SerieModel.cs
@@ -1,24 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Primitives;
 using WebApp.Dto;
 
 namespace WebApp.Models
 {
-    public class SerieModel
-    {
-        public SerieDto Serie { get; set; }
-        public List<SerieDto> Series { get; set; }
-        public string SerieId { get; set; }
-        public SelectList ListSeries { get; set; }
+	public class SerieModel
+	{
+		public SerieDto Serie { get; set; }
+		public List<SerieDto> Series { get; set; }
+		public string SerieId { get; set; }
+		public SelectList ListSeries { get; set; }
 
-        public class CreateUpdateSerieCommand
-        {
-            public int Id { get; set; }
-            public required string Nome { get; set; }
-            public required string Descricao { get; set; }
-            public required int IdadeInicial { get; set; }
-            public required int IdadeFinal { get; set; }
-            public required int ScoreTotal { get; set; }
-        }
-    }
+		public class CreateUpdateSerieCommand
+		{
+			public int Id { get; set; }
+			public string Nome { get; set; }
+			public bool Status { get; set; }
+		}
+	}
 
 }
