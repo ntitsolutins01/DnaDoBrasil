788d6a3f41d96a21f7c073505228b5bf952af819
FabioMuniz <ntitsolutions01@gmail.com>
2023-12-18 15:15:30 -0300

    Auto stash before merge of "feature/dev-drk" and "origin/feature/dev-drk"

--- WebApp/wwwroot/js/tipo-laudo.js
+++ WebApp/wwwroot/js/tipo-laudo.js
@@ -9,6 +9,17 @@ var vm = new Vue({
         (function ($) {
             'use strict';
 
+            if (typeof Switch !== 'undefined' && $.isFunction(Switch)) {
+
+                $(function () {
+                    $('[data-plugin-ios-switch]').each(function () {
+                        var $this = $(this);
+
+                        $this.themePluginIOS7Switch();
+                    });
+                });
+            }
+
             var formid = $('form').attr('id');
 
             if (formid === "formEditTiposLaudo") {
