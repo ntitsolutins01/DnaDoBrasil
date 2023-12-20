9027b641a1bffbe9324171a40d9684778a874fcc
FabioMuniz <ntitsolutions01@gmail.com>
2023-12-20 10:05:45 -0300

    chore: consulta parceiro

--- WebApp/Controllers/SistemaSocioeconomicoController.cs
+++ WebApp/Controllers/SistemaSocioeconomicoController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using WebApp.Configuration;
+using WebApp.Dto;
 using WebApp.Enumerators;
 using WebApp.Factory;
 using WebApp.Models;
@@ -10,141 +11,156 @@ namespace WebApp.Controllers
 {
 
     public class SistemaSocioeconomicoController : BaseController
-	{
-		private readonly IOptions<UrlSettings> _appSettings;
+    {
+        private readonly IOptions<UrlSettings> _appSettings;
 
-		public SistemaSocioeconomicoController(IOptions<UrlSettings> appSettings)
-		{
-			_appSettings = appSettings;
-			ApplicationSettings.WebApiUrl = _appSettings.Value.WebApiBaseUrl;
-		}
+        public SistemaSocioeconomicoController(IOptions<UrlSettings> appSettings)
+        {
+            _appSettings = appSettings;
+            ApplicationSettings.WebApiUrl = _appSettings.Value.WebApiBaseUrl;
+        }
 
-		public IActionResult Parceiro(int? crud, int? notify, string message = null)
-		{
-			SetNotifyMessage(notify, message);
-			SetCrudMessage(crud);
-			//var response = ApiClientFactory.Instance.GetParceiroAll();
+        public IActionResult Parceiro(int? crud, int? notify, string message = null)
+        {
+            try
+            {
+                SetNotifyMessage(notify, message);
+                SetCrudMessage(crud);
+                var response = ApiClientFactory.Instance.GetParceiroAll();
 
-			//return View(new ParceiroModel(){Parceiros = response});
-			return View();
-		}
-		
-		//[ClaimsAuthorize("ConfiguracaoSistema", "Incluir")]
-		public ActionResult CreateParceiro(int? crud, int? notify, string message = null)
-		{
-			SetNotifyMessage(notify, message);
-			SetCrudMessage(crud);
+                return View(new ParceiroModel() { Parceiros = response });
 
-			return View();
-		}
-		//[ClaimsAuthorize("Usuario", "Incluir")]
-		[HttpPost]
-		public async Task<ActionResult> Create(IFormCollection collection)
-		{
-			try
-			{
-				var command = new ParceiroModel.CreateUpdateParceiroCommand
-				{
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.StackTrace);
+                return RedirectToAction(nameof(Parceiro), new { notify = (int)EnumNotify.Error, message = e.Message });
 
-					Nome = collection["nome"].ToString(),
-					TipoPessoa = collection["TipoPessoa"].ToString(),
-					CpfCnpj = collection["cpfCnpj"].ToString(),
-					Telefone = collection["Telefone"].ToString(),
-					Celular = collection["Celular"].ToString(),
-					Endereco = collection["Endereço"].ToString(),
-					Numero = Convert.ToInt32(collection["numero"].ToString()),
-					Bairro = collection["Bairro"].ToString(),
-					Habilitado = collection["habilitado"].ToString() == "1"? true:false, //TODO: Verificar condicional para resgate radio button
-					Status = collection["status"].ToString() == "1"? true:false,
-					Email = collection["Email"].ToString(),
-					TipoParceria = Convert.ToInt32(collection["TipoParceria"].ToString()), //TODO: Verificar condicional para resgate checkbox
-				};
+            }
+        }
 
-				await ApiClientFactory.Instance.CreateParceiro(command);
+        //[ClaimsAuthorize("ConfiguracaoSistema", "Incluir")]
+        public ActionResult CreateParceiro(int? crud, int? notify, string message = null)
+        {
+            SetNotifyMessage(notify, message);
+            SetCrudMessage(crud);
 
-				return RedirectToAction(nameof(Parceiro), new { crud = (int)EnumCrud.Created });
-			}
-			catch (Exception e)
-			{
-				return RedirectToAction(nameof(Parceiro));
-			}
-		}
-		//[ClaimsAuthorize("Parceiro", "Alterar")]
-		//public ActionResult Edit(string id)
-		//{
-		//    var obj = ApiClientFactory.Instance.GetParceiroById(id);
+            return View();
+        }
+        //[ClaimsAuthorize("Usuario", "Incluir")]
+        [HttpPost]
+        public async Task<ActionResult> Create(IFormCollection collection)
+        {
+            try
+            {
+                var command = new ParceiroModel.CreateUpdateParceiroCommand
+                {
 
-		//    var model = new ParceiroModel() { Parceiro = obj };
+                    Nome = collection["nome"].ToString(),
+                    TipoPessoa = collection["TipoPessoa"].ToString(),
+                    CpfCnpj = collection["cpfCnpj"].ToString(),
+                    Telefone = collection["Telefone"].ToString(),
+                    Celular = collection["Celular"].ToString(),
+                    Endereco = collection["Endereço"].ToString(),
+                    Numero = Convert.ToInt32(collection["numero"].ToString()),
+                    Bairro = collection["Bairro"].ToString(),
+                    Habilitado = collection["habilitado"].ToString() == "1" ? true : false, //TODO: Verificar condicional para resgate radio button
+                    Status = collection["status"].ToString() == "1" ? true : false,
+                    Email = collection["Email"].ToString(),
+                    TipoParceria = Convert.ToInt32(collection["TipoParceria"].ToString()), //TODO: Verificar condicional para resgate checkbox
+                };
 
-		//    return View(model);
-		//}
-		//[ClaimsAuthorize("Usuario", "Alterar")]
-		public async Task<ActionResult> Edit(string id, IFormCollection collection)
-		{
-			var command = new ParceiroModel.CreateUpdateParceiroCommand
-			{
-				Id = Convert.ToInt32(id),
-				Nome = collection["nome"].ToString(),
-				TipoPessoa = collection["TipoPessoa"].ToString(),
-				CpfCnpj = collection["cpfCnpj"].ToString(),
-				Telefone = collection["Telefone"].ToString(),
-				Celular = collection["Celular"].ToString(),
-				Endereco = collection["Endereço"].ToString(),
-				Numero = Convert.ToInt32(collection["numero"].ToString()),
-				Bairro = collection["Bairro"].ToString(),
-				Habilitado = collection["habilitado"].ToString() == "1" ? true : false, //TODO: Verificar condicional para resgate radio button
-				Status = collection["status"].ToString() == "1" ? true : false,
-				Email = collection["Email"].ToString(),
-				TipoParceria = Convert.ToInt32(collection["TipoParceria"].ToString()), //TODO: Verificar condicional para resgate checkbox
-			};
+                await ApiClientFactory.Instance.CreateParceiro(command);
 
-			//await ApiClientFactory.Instance.UpdateParceiro(command);
+                return RedirectToAction(nameof(Parceiro), new { crud = (int)EnumCrud.Created });
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction(nameof(Parceiro));
+            }
+        }
+        //[ClaimsAuthorize("Parceiro", "Alterar")]
+        //public ActionResult Edit(string id)
+        //{
+        //    var obj = ApiClientFactory.Instance.GetParceiroById(id);
 
-			return RedirectToAction(nameof(Parceiro), new { crud = (int)EnumCrud.Updated });
-		}
+        //    var model = new ParceiroModel() { Parceiro = obj };
 
-		//[ClaimsAuthorize("Usuario", "Excluir")]
-		public ActionResult Delete(string id)
-		{
-			try
-			{
-				//ApiClientFactory.Instance.DeleteParceiro(id);
-				return RedirectToAction(nameof(Parceiro), new { crud = (int)EnumCrud.Deleted });
-			}
-			catch
-			{
-				return RedirectToAction(nameof(Parceiro));
-			}
-		}
+        //    return View(model);
+        //}
+        //[ClaimsAuthorize("Usuario", "Alterar")]
+        public async Task<ActionResult> Edit(string id, IFormCollection collection)
+        {
+            var command = new ParceiroModel.CreateUpdateParceiroCommand
+            {
+                Id = Convert.ToInt32(id),
+                Nome = collection["nome"].ToString(),
+                TipoPessoa = collection["TipoPessoa"].ToString(),
+                CpfCnpj = collection["cpfCnpj"].ToString(),
+                Telefone = collection["Telefone"].ToString(),
+                Celular = collection["Celular"].ToString(),
+                Endereco = collection["Endereço"].ToString(),
+                Numero = Convert.ToInt32(collection["numero"].ToString()),
+                Bairro = collection["Bairro"].ToString(),
+                Habilitado = collection["habilitado"].ToString() == "1" ? true : false, //TODO: Verificar condicional para resgate radio button
+                Status = collection["status"].ToString() == "1" ? true : false,
+                Email = collection["Email"].ToString(),
+                TipoParceria = Convert.ToInt32(collection["TipoParceria"].ToString()), //TODO: Verificar condicional para resgate checkbox
+            };
 
+            //await ApiClientFactory.Instance.UpdateParceiro(command);
 
+            return RedirectToAction(nameof(Parceiro), new { crud = (int)EnumCrud.Updated });
+        }
 
+        //[ClaimsAuthorize("Usuario", "Excluir")]
+        public ActionResult Delete(string id)
+        {
+            try
+            {
+                //ApiClientFactory.Instance.DeleteParceiro(id);
+                return RedirectToAction(nameof(Parceiro), new { crud = (int)EnumCrud.Deleted });
+            }
+            catch
+            {
+                return RedirectToAction(nameof(Parceiro));
+            }
+        }
 
+        public async Task<ParceiroDto> GetParceiroById(int id)
+        {
+            var result = ApiClientFactory.Instance.GetParceiroById(id);
 
-
-
-
-
-
-
-
-
-
-
-
+            return result;
+        }
 
 
-		public IActionResult Estudantes()
-		{
-			return View();
-		}
-		public IActionResult SolicitacaoContato()
-		{
-			return View();
-		}
-		public IActionResult Details()
-		{
-			return View();
-		}
-	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public IActionResult Estudantes()
+        {
+            return View();
+        }
+        public IActionResult SolicitacaoContato()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
 }
--- WebApp/wwwroot/js/parceiro.js
+++ WebApp/wwwroot/js/parceiro.js
@@ -1,5 +1,5 @@
 ﻿var vm = new Vue({
-    el: "#formSerie",
+    el: "#formParceiro",
     data: {},
     mounted: function () {
         var self = this;
@@ -7,7 +7,7 @@
 
             'use strict';
 
-            $("#formSerie").validate({
+            $("#formParceiro").validate({
                 highlight: function (label) {
                     $(label).closest('.form-group').removeClass('has-success').addClass('has-error');
                 },
@@ -26,7 +26,7 @@
                 }
             });
 
-            $("#formEditSerie").validate({
+            $("#formEditParceiro").validate({
                 highlight: function (label) {
                     $(label).closest('.form-group').removeClass('has-success').addClass('has-error');
                 },
@@ -65,6 +65,77 @@
                 $("#" + el).addClass("loading-overlay-showing");
                 self.loading = flag;
             }
+        },
+        EditPendentes: function (id) {
+            var self = this;
+
+            axios.get("Parceiro/GetParceiroById/?id=" + id).then(result => {
+
+                self.editDto.Id = result.data.id;
+
+                var alunos = result.data.alunos;
+
+                $('#alunosSolicitados').DataTable().destroy();
+
+                $('#alunosSolicitados').DataTable({
+                    data: alunos,
+                    "columns": [
+                        { "data": "nome" },
+                        { "data": "idade" },
+                        { "data": "talento" }
+                    ],
+                    "paging": false,
+                    "searching": false,
+                    "language": {
+                        "sEmptyTable": "Nenhum registro encontrado",
+                        "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+                        "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+                        "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+                        "sInfoPostFix": "",
+                        "sInfoThousands": ".",
+                        "sLengthMenu": "_MENU_ resultados por página",
+                        "sLoadingRecords": "Carregando...",
+                        "sProcessing": "Processando...",
+                        "sZeroRecords": "Nenhum registro encontrado",
+                        "sSearch": "Pesquisar: ",
+                        "oPaginate": {
+                            "sNext": "Próximo →" +
+                                "" +
+                                "",
+                            "sPrevious": "← Anterior",
+                            "sFirst": "Primeiro",
+                            "sLast": "Último"
+                        },
+                        "oAria": {
+                            "sSortAscending": ": Ordenar colunas de forma ascendente",
+                            "sSortDescending": ": Ordenar colunas de forma descendente"
+                        }
+                    }
+                });
+
+                $('#alunosSolicitados').DataTable().draw();
+
+            }).catch(error => {
+                Site.Notification("Erro ao buscar e analisar dados", error.message, "error", 1);
+            });
         }
     }
 });
+
+var crud = {
+    DeleteModal: function (id) {
+        $('input[name="ParceiroId"]').attr('value', id);
+        $('#mdDeleteParceiro').modal('show');
+        vm.DeleteParceiro(id)
+    },
+    EditModal: function (id) {
+        $('input[name="ParceiroId"]').attr('value', id);
+        $('#mdEditParceiro').modal('show');
+        vm.EditParceiro(id)
+    },
+    PendentesModal: function (id) {
+        $('input[name="ParceiroId"]').attr('value', id);
+        $('#mdEditParceiro').modal('show');
+        vm.EditPendentes(id)
+    }
+};
