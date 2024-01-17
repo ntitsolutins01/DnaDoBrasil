f8f8aee8505dfee324614c1b755be374ec044eb7
Fabio Muniz <fabio.muniz01@gmail.com>
2024-01-17 09:49:13 -0300

    chpre: dependencia dto e model

---
+++ WebApp/Models/DependenciaModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApp.Dto;
+
+namespace WebApp.Models
+{
+    public class DependenciaModel
+    {
+        public DependenciaDto Dependencia { get; set; }
+        public List<DependenciaDto> Dependencias { get; set; }
+
+        public class CreateUpdateDependenciaCommand
+        {
+            public int Id { get; set; }
+            public string? Doencas { get; set; }
+            public string? Nacionalidade { get; set; }
+            public string? Naturalidade { get; set; }
+            public string? NomeEscola { get; set; }
+            public int TipoEscola { get; set; }
+            public int TipoEscolaridade { get; set; }
+            public string? Turno { get; set; }
+            public string? Serie { get; set; }
+            public string? Ano { get; set; }
+            public string? Turma { get; set; }
+            public bool? TermoCompromisso { get; set; }
+            public bool? AutorizacaoUsoImagemAudio { get; set; }
+            public bool? AutorizacaoUsoIndicadores { get; set; }
+            public bool? AutorizacaoSaida { get; set; } = false;
+            public int? AlunoId { get; set; }
+        }
+    }
+
+}
