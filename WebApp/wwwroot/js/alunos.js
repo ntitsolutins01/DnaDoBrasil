5fd2065b7a7026054ed6da79d2d5d388bf920930
Fabio Muniz <fabio.muniz01@gmail.com>
2024-01-29 16:05:31 -0300

    chore: correcao pesquisa combos de aluno

--- WebApp/wwwroot/js/alunos.js
+++ WebApp/wwwroot/js/alunos.js
@@ -1,5 +1,5 @@
 ﻿var vm = new Vue({
-    el: "#formAluno",
+    el: "#formPesquisarAluno",
     data: {
         loading: false
     },
@@ -8,49 +8,15 @@
         (function ($) {
             'use strict';
 
-            if (typeof Switch !== 'undefined' && $.isFunction(Switch)) {
-
-                $(function () {
-                    $('[data-plugin-ios-switch]').each(function () {
-                        var $this = $(this);
-
-                        $this.themePluginIOS7Switch();
-                    });
-                });
-            }
-
             var formid = $('form').attr('id');
 
-            if (formid === "formAluno") {
-                var $select = $(".select2").select2({
-                    allowClear: true
-                });
-
-                $(".select2").each(function () {
-                    var $this = $(this),
-                        opts = {};
-
-                    var pluginOptions = $this.data('plugin-options');
-                    if (pluginOptions)
-                        opts = pluginOptions;
-
-                    $this.themePluginSelect2(opts);
-                });
-
-                /*
-                 * When you change the value the select via select2, it triggers
-                 * a 'change' event, but the jquery validation plugin
-                 * only re-validates on 'blur'*/
-
-                $select.on('change', function () {
-                    $(this).trigger('blur');
-                });
+            if (formid === "formPesquisarAluno") {
 
                 //clique de escolha do select
                 $("#ddlEstado").change(function () {
                     var sigla = $("#ddlEstado").val();
 
-                    var url = "../DivisaoAdministrativa/GetMunicipioByUf?uf=" + sigla;
+                    var url = "../../DivisaoAdministrativa/GetMunicipioByUf?uf=" + sigla;
 
                     var ddlSource = "#ddlMunicipio";
 
@@ -76,78 +42,35 @@
                         });
                 });
 
-                //mascara dos inputs
-                var $numCpf = $("#cpf");
-                $numCpf.mask('000.000.000-00', { reverse: false });
-                jQuery.validator.addMethod("cpf", function (cpf, element) {
-                    var regex = /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/;
-                    var add, rev, i;
-                    if (!regex.test(cpf))
-                        return false;
+                //clique de escolha do select
+                $("#ddlMunicipio").change(function () {
+                    var id = $("#ddlMunicipio").val();
 
-                    cpf = cpf.replace(/[^\d]+/g, '');
-                    if (cpf == '') return false;
-                    // Elimina CPFs invalidos conhecidos	
-                    if (cpf.length != 11 ||
-                        cpf == "00000000000" ||
-                        cpf == "11111111111" ||
-                        cpf == "22222222222" ||
-                        cpf == "33333333333" ||
-                        cpf == "44444444444" ||
-                        cpf == "55555555555" ||
-                        cpf == "66666666666" ||
-                        cpf == "77777777777" ||
-                        cpf == "88888888888" ||
-                        cpf == "99999999999")
-                        return false;
-                    // Valida 1o digito	
-                    add = 0;
-                    for (i = 0; i < 9; i++)
-                        add += parseInt(cpf.charAt(i)) * (10 - i);
-                    rev = 11 - (add % 11);
-                    if (rev == 10 || rev == 11)
-                        rev = 0;
-                    if (rev != parseInt(cpf.charAt(9)))
-                        return false;
-                    // Valida 2o digito	
-                    add = 0;
-                    for (i = 0; i < 10; i++)
-                        add += parseInt(cpf.charAt(i)) * (11 - i);
-                    rev = 11 - (add % 11);
-                    if (rev == 10 || rev == 11)
-                        rev = 0;
-                    if (rev != parseInt(cpf.charAt(10)))
-                        return false;
-                    return true;
+                    var url = "../../Localidade/GetLocalidadeByMunicipio?id=" + id;
 
+                    var ddlSource = "#ddlLocalidade";
 
-                }, "Informe um CPF válido");
-
-
-                    $("#formAluno").validate({
-                        rules: {
-                            cpf: { cpf: true, required: true }
-                        },
-                        messages: {
-                            cpf: { cpf: 'Formato de CPF inválido', required: "Por favor informe o número do CPF do profissional." }
-                        },
-                        highlight: function (label) {
-                            $(label).closest('.form-group').removeClass('has-success').addClass('has-error');
-                        },
-                        success: function (label) {
-                            $(label).closest('.form-group').removeClass('has-error');
-                            label.remove();
-                        },
-                        errorPlacement: function (error, element) {
-                            var placement = element.closest('.input-group');
-                            if (!placement.get(0)) {
-                                placement = element;
+                    $.getJSON(url,
+                        { id: $(ddlSource).val() },
+                        function (data) {
+                            if (data.length > 0) {
+                                var items = '<option value="">Selecionar Localidade</option>';
+                                $("#ddlLocalidade").empty;
+                                $.each(data,
+                                    function (i, row) {
+                                        items += "<option value='" + row.value + "'>" + row.text + "</option>";
+                                    });
+                                $("#ddlLocalidade").html(items);
                             }
-                            if (error.text() !== '') {
-                                placement.after(error);
+                            else {
+                                new PNotify({
+                                    title: 'Localidades',
+                                    text: 'Localidades não encontradas.',
+                                    type: 'warning'
+                                });
                             }
-                        }
-                    });
+                        });
+                });
             }
         }).apply(this, [jQuery]);
     },
