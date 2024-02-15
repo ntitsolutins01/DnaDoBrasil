9433a93e648424ed2441faaab790a884d7549772
Fabio Muniz <fabio.muniz01@gmail.com>
2024-02-15 16:19:21 -0300

    chore? ajustando erros

--- WebApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WebApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -93,12 +93,16 @@ namespace WebApp.Areas.Identity.Pages.Account
 
         }
 
-        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        public async Task<IActionResult> OnPostAsync(IFormCollection collection)
         {
+            string returnUrl = null;
             returnUrl ??= Url.Content("~/");
 
+
+
+
             if (!ModelState.IsValid) return Page();
-            var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+            var user = new IdentityUser { UserName = Input.Email, Email = Input.Email, EmailConfirmed = true, PhoneNumberConfirmed = true};
             var result = await _userManager.CreateAsync(user, Input.Password);
             if (!result.Succeeded)
             {
