42c87cb9757ee54ab649c8aaae6eff80089c9ae9
DRKinfo <126280960+DRKinfo@users.noreply.github.com>
2024-02-16 19:04:30 -0300

    chore:implementação de value no edit de laudo

--- WebApp/Dto/LaudoDto.cs
+++ WebApp/Dto/LaudoDto.cs
@@ -9,6 +9,13 @@
         public ConsumoAlimentarDto ConsumoAlimentar { get; set; }
         public SaudeBucalDto SaudeBucal { get; set; }
         public  AlunoDto Aluno { get; set; }
-        public string Id { get; set; }
+        public int Id { get; set; }
+        public string Altura { get; set; }
+        public string MassaCorporal { get; set; }
+        public object Envergadura { get; set; }
+        public object PreensaoManual { get; set; }
+        public object Flexibilidade { get; set; }
+        public object ImpulsaoHorizontal { get; set; }
+        public object AptidaoFísica { get; set; }
     }
 }
