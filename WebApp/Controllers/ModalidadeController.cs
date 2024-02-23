6ac9bda276371860885ce24731057e19419c0889
Fabio Muniz <fabio.muniz01@gmail.com>
2024-02-23 18:55:53 -0300

    chore: implementacao de modalidade cadastro alteracao e exclusao OK

--- WebApp/Controllers/ModalidadeController.cs
+++ WebApp/Controllers/ModalidadeController.cs
@@ -46,7 +46,27 @@ namespace WebApp.Controllers
                 var command = new ModalidadeModel.CreateUpdateModalidadeCommand
                 {
                     Nome = collection["nome"].ToString(),
-                    AbdominalPranchaIni = Convert.ToInt32(collection["nome"].ToString())
+                    Vo2MaxIni = Convert.ToInt32(collection["vo2MaxIni"].ToString()),
+                    Vo2MaxFim = Convert.ToInt32(collection["vo2MaxFim"].ToString()),
+                    VinteMetrosIni = Convert.ToInt32(collection["vinteMetrosIni"].ToString()),
+                    VinteMetrosFim = Convert.ToInt32(collection["vinteMetrosFim"].ToString()),
+                    ShutlleRunIni = Convert.ToInt32(collection["shutlleRunIni"].ToString()),
+                    ShutlleRunFim = Convert.ToInt32(collection["shutlleRunFim"].ToString()),
+                    FlexibilidadeIni = Convert.ToInt32(collection["flexibilidadeIni"].ToString()),
+                    FlexibilidadeFim = Convert.ToInt32(collection["flexibilidadeFim"].ToString()),
+                    PreensaoManualIni = Convert.ToInt32(collection["preensaoManualIni"].ToString()),
+                    PreensaoManualFim = Convert.ToInt32(collection["preensaoManualFim"].ToString()),
+                    AbdominalPranchaIni = Convert.ToInt32(collection["abdominalPranchaIni"].ToString()),
+                    AbdominalPranchaFim = Convert.ToInt32(collection["abdominalPranchaFim"].ToString()),
+                    ImpulsaoIni = Convert.ToInt32(collection["impulsaoIni"].ToString()),
+                    ImpulsaoFim = Convert.ToInt32(collection["impulsaoFim"].ToString()),
+                    EnvergaduraIni = Convert.ToInt32(collection["envergaduraIni"].ToString()),
+                    EnvergaduraFim = Convert.ToInt32(collection["envergaduraFim"].ToString()),
+                    PesoIni = Convert.ToInt32(collection["pesoIni"].ToString()),
+                    PesoFim = Convert.ToInt32(collection["pesoFim"].ToString()),
+                    AlturaIni = Convert.ToInt32(collection["alturaIni"].ToString()),
+                    AlturaFim = Convert.ToInt32(collection["alturaFim"].ToString()),
+                    Status = true
                 };
 
                 await ApiClientFactory.Instance.CreateModalidade(command);
@@ -66,6 +86,26 @@ namespace WebApp.Controllers
             {
                 Id = Convert.ToInt32(collection["editModalidadeId"]),
                 Nome = collection["nome"].ToString(),
+                Vo2MaxIni = Convert.ToInt32(collection["vo2MaxIni"].ToString()),
+                Vo2MaxFim = Convert.ToInt32(collection["vo2MaxFim"].ToString()),
+                VinteMetrosIni = Convert.ToInt32(collection["vinteMetrosIni"].ToString()),
+                VinteMetrosFim = Convert.ToInt32(collection["vinteMetrosFim"].ToString()),
+                ShutlleRunIni = Convert.ToInt32(collection["shutlleRunIni"].ToString()),
+                ShutlleRunFim = Convert.ToInt32(collection["shutlleRunFim"].ToString()),
+                FlexibilidadeIni = Convert.ToInt32(collection["flexibilidadeIni"].ToString()),
+                FlexibilidadeFim = Convert.ToInt32(collection["flexibilidadeFim"].ToString()),
+                PreensaoManualIni = Convert.ToInt32(collection["preensaoManualIni"].ToString()),
+                PreensaoManualFim = Convert.ToInt32(collection["preensaoManualFim"].ToString()),
+                AbdominalPranchaIni = Convert.ToInt32(collection["abdominalPranchaIni"].ToString()),
+                AbdominalPranchaFim = Convert.ToInt32(collection["abdominalPranchaFim"].ToString()),
+                ImpulsaoIni = Convert.ToInt32(collection["impulsaoIni"].ToString()),
+                ImpulsaoFim = Convert.ToInt32(collection["impulsaoFim"].ToString()),
+                EnvergaduraIni = Convert.ToInt32(collection["envergaduraIni"].ToString()),
+                EnvergaduraFim = Convert.ToInt32(collection["envergaduraFim"].ToString()),
+                PesoIni = Convert.ToInt32(collection["pesoIni"].ToString()),
+                PesoFim = Convert.ToInt32(collection["pesoFim"].ToString()),
+                AlturaIni = Convert.ToInt32(collection["alturaIni"].ToString()),
+                AlturaFim = Convert.ToInt32(collection["alturaFim"].ToString()),
                 Status = collection["editStatus"].ToString() == "" ? false : true
             };
 
