5fa7a0b42ef543c65245a62e18f6ea56094cab8f
drkinfo616@gmail.com <drkinfo616@gmail.com>
2024-07-04 18:08:15 -0300

    chore: implementacao de modulo ead

---
+++ WebApp/Dto/ModuloEadDto.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApp.Dto
+{
+    public class ModuloEadDto
+    {
+		public required int Id { get; set; }
+		public required int CargaHoraria { get; set; }
+		public required int CursoId { get; set; }
+		public required string Titulo { get; set; }
+		public string? Descricao { get; set; }
+		public bool Status { get; set; }
+        public required string NomeTipoCurso { get; set; }
+    }
+}
