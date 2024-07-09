62217c329a36de0158a09ce9560b9824ffa910d9
drkinfo616@gmail.com <drkinfo616@gmail.com>
2024-07-09 20:30:12 -0300

    chore: implementacao de encaminhamento

---
+++ WebApp/wwwroot/js/Encaminhamento.js
@@ -0,0 +1,303 @@
+var vm = new Vue({
+    el: "#vEncaminhamento",
+    data: {
+        loading: false,
+        editDto: { Id: "", Nome: "", Parametro: "", Descricao: "", Status: true }
+    },
+    mounted: function () {
+        var self = this;
+        (function ($) {
+            'use strict';
+
+           
+            //skin checkbox
+            if (typeof Switch !== 'undefined' && $.isFunction(Switch)) {
+
+                $(function () {
+                    $('[data-plugin-ios-switch]').each(function () {
+                        var $this = $(this);
+
+                        $this.themePluginIOS7Switch();
+                    });
+                });
+            }
+
+            var formid = $('form')[1].id;
+
+            if (formid === "formEditEncaminhamento") {
+
+                $("#formEditEncaminhamento").validate({
+                    rules:
+                    {
+                        EncaminhamentoPrimeiroBimestre:
+                        {
+                            range: [0.00, 10]
+                        },
+                        EncaminhamentoSegundoBimestre:
+                        {
+                            range: [0.00, 10]
+                        },
+                        EncaminhamentoTerceiroBimestre:
+                        {
+                            range: [0.00, 10]
+                        },
+                        EncaminhamentoQuartoBimestre:
+                        {
+                            range: [0.00, 10]
+                        }
+                    },
+                    messages: {
+                        "EncaminhamentoPrimeiroBimestre": {
+                            range: "Encaminhamento deve ser entre 00,00 à 10."
+                        },
+                        "EncaminhamentoSegundoBimestre": {
+                            range: "Encaminhamento deve ser entre 00,00 à 10."
+                        },
+                        "EncaminhamentoTerceiroBimestre": {
+                            range: "Encaminhamento deve ser entre 00,00 à 10."
+                        },
+                        "EncaminhamentoQuartoBimestre": {
+                            range: "Encaminhamento deve ser entre 00,00 à 10."
+                        }
+                    },
+                    highlight: function (label) {
+                        $(label).closest('.form-group').removeClass('has-success').addClass('has-error');
+                    },
+                    success: function (label) {
+                        $(label).closest('.form-group').removeClass('has-error');
+                        label.remove();
+                    },
+                    errorPlacement: function (error, element) {
+                        var placement = element.closest('.input-group');
+                        if (!placement.get(0)) {
+                            placement = element;
+                        }
+                        if (error.text() !== '') {
+                            placement.after(error);
+                        }
+                    }
+                });
+            }
+
+            if (formid === "formEncaminhamento") {
+                //skin select
+                var $select = $(".select2").select2({
+                    allowClear: true
+                });
+
+                $(".select2").each(function () {
+                    var $this = $(this),
+                        opts = {};
+
+                    var pluginOptions = $this.data('plugin-options');
+                    if (pluginOptions)
+                        opts = pluginOptions;
+
+                    $this.themePluginSelect2(opts);
+                });
+
+                /*
+                 * When you change the value the select via select2, it triggers
+                 * a 'change' event, but the jquery validation plugin
+                 * only re-validates on 'blur'*/
+
+                $select.on('change', function () {
+                    $(this).trigger('blur');
+                });
+
+                $("#ddlEstado").change(function () {
+                    var sigla = $("#ddlEstado").val();
+
+                    var url = "../DivisaoAdministrativa/GetMunicipioByUf?uf=" + sigla;
+
+                    var ddlSource = "#ddlMunicipio";
+
+                    $.getJSON(url,
+                        { id: $(ddlSource).val() },
+                        function (data) {
+                            if (data.length > 0) {
+                                var items = '<option value="">Selecionar Municipio</option>';
+                                $("#ddlMunicipio").empty;
+                                $.each(data,
+                                    function (i, row) {
+                                        items += "<option value='" + row.value + "'>" + row.text + "</option>";
+                                    });
+                                $("#ddlMunicipio").html(items);
+                            }
+                            else {
+                                new PNotify({
+                                    title: 'Fomento',
+                                    text: 'Municípios não encontrados.',
+                                    type: 'warning'
+                                });
+                            }
+                        });
+                });
+                //clique de escolha do select
+                $("#ddlMunicipio").change(function () {
+                    var id = $("#ddlMunicipio").val();
+
+                    var url = "../../Localidade/GetLocalidadeByMunicipio?id=" + id;
+
+                    var ddlSource = "#ddlLocalidade";
+
+                    $.getJSON(url,
+                        { id: $(ddlSource).val() },
+                        function (data) {
+                            if (data.length > 0) {
+                                var items = '<option value="">Selecionar Localidade</option>';
+                                $("#ddlLocalidade").empty;
+                                $.each(data,
+                                    function (i, row) {
+                                        items += "<option value='" + row.value + "'>" + row.text + "</option>";
+                                    });
+                                $("#ddlLocalidade").html(items);
+                            }
+                            else {
+                                new PNotify({
+                                    title: 'Localidades',
+                                    text: 'Localidades não encontradas.',
+                                    type: 'warning'
+                                });
+                            }
+                        });
+                });
+                //clique de escolha do select
+                $("#ddlLocalidade").change(function () {
+                    var id = $("#ddlLocalidade").val();
+
+                    var url = "../../Aluno/GetAlunosByLocalidade?id=" + id;
+
+                    var ddlSource = "#ddlAluno";
+
+                    $.getJSON(url,
+                        { id: $(ddlSource).val() },
+                        function (data) {
+                            if (data.length > 0) {
+                                var items = '<option value="">Selecionar Aluno</option>';
+                                $("#ddlAluno").empty;
+                                $.each(data,
+                                    function (i, row) {
+                                        items += "<option value='" + row.value + "'>" + row.text + "</option>";
+                                    });
+                                $("#ddlAluno").html(items);
+                            }
+                            else {
+                                new PNotify({
+                                    title: 'Alunos',
+                                    text: 'Alunos não encontrados.',
+                                    type: 'warning'
+                                });
+                            }
+                        });
+                });
+
+                $("#formEncaminhamento").validate({
+                    rules:
+                    {
+                        EncaminhamentoPrimeiroBimestre:
+                        {
+                            range: [0.00, 10]
+                        },
+                        EncaminhamentoSegundoBimestre:
+                        {
+                            range: [0.00, 10]
+                        },
+                        EncaminhamentoTerceiroBimestre:
+                        {
+                            range: [0.00, 10]
+                        },
+                        EncaminhamentoQuartoBimestre:
+                        {
+                            range: [0.00, 10]
+                        }
+                    },
+                    messages: {
+                        "EncaminhamentoPrimeiroBimestre": {
+                            range: "Encaminhamento deve ser entre 00,00 à 10."
+                        },
+                        "EncaminhamentoSegundoBimestre": {
+                            range: "Encaminhamento deve ser entre 00,00 à 10."
+                        },
+                        "EncaminhamentoTerceiroBimestre": {
+                            range: "Encaminhamento deve ser entre 00,00 à 10."
+                        },
+                        "EncaminhamentoQuartoBimestre": {
+                            range: "Encaminhamento deve ser entre 00,00 à 10."
+                        }
+                    },
+                    highlight: function (label) {
+                        $(label).closest('.form-group').removeClass('has-success').addClass('has-error');
+                    },
+                    success: function (label) {
+                        $(label).closest('.form-group').removeClass('has-error');
+                        label.remove();
+                    },
+                    errorPlacement: function (error, element) {
+                        var placement = element.closest('.input-group');
+                        if (!placement.get(0)) {
+                            placement = element;
+                        }
+                        if (error.text() !== '') {
+                            placement.after(error);
+                        }
+                    }
+                });
+            }
+        }).apply(this, [jQuery]);
+    },
+    methods: {
+        ShowLoad: function (flag, el) {
+            var self = this;
+
+            self.isLoading = flag;
+            $("#" + el).loadingOverlay({
+                "startShowing": flag
+            });
+            self.loading = flag;
+
+            if (!flag) {
+                self.isLoading = flag;
+                $("#" + el).removeClass("loading-overlay-showing");
+                self.loading = flag;
+            } else {
+                self.isLoading = flag;
+                $("#" + el).addClass("loading-overlay-showing");
+                self.loading = flag;
+            }
+        },
+        DeleteEncaminhamento: function (id) {
+            var url = "Encaminhamento/Delete/" + id;
+            $("#deleteEncaminhamentoHref").prop("href", url);
+        },
+        EditEncaminhamento: function (id) {
+            var self = this;
+
+            axios.get("Encaminhamento/GetEncaminhamentoById/?id=" + id).then(result => {
+
+                self.editDto.Id = result.data.id;
+                self.editDto.Nome = result.data.nome;
+                self.editDto.Parametro = result.data.paremetro;
+                self.editDto.Descricao = result.data.descricao;
+                self.editDto.Status = result.data.status;
+                
+
+            }).catch(error => {
+                Site.Notification("Erro ao buscar e analisar dados", error.message, "error", 1);
+            });
+        }
+    }
+});
+
+var crud = {
+    DeleteModal: function (id) {
+        $('input[name="deleteEncaminhamentoId"]').attr('value', id);
+        $('#mdDeleteEncaminhamento').modal('show');
+        vm.DeleteEncaminhamento(id)
+    },
+    EditModal: function (id) {
+        $('input[name="editEncaminhamentoId"]').attr('value', id);
+        $('#mdEditEncaminhamento').modal('show');
+        vm.EditEncaminhamento(id)
+    }
+};
