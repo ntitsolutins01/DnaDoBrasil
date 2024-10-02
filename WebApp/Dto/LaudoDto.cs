65e4891153cb12d4135d9ac30fa963fe882eefdc
Lutercio Jackson <luterciofilho11@hotmail.com>
2024-10-02 20:14:19 -0300

    chore: client GetEncaminhamentoBySaudeId + LaudoDto

--- WebApp/Dto/LaudoDto.cs
+++ WebApp/Dto/LaudoDto.cs
@@ -2,20 +2,52 @@
 {
     public class LaudoDto
     {
-        public int? Id { get; set; }
-        public int? TalentoEsportivoId { get; set; }
-        public int? VocacionalId { get; set; }
-        public int? QualidadeDeVidaId { get; set; }
-        public int? SaudeId { get; set; }
-        public int? ConsumoAlimentarId { get; set; }
-        public int? SaudeBucalId { get; set; }
-        public int? AlunoId { get; set; }
-        public required string NomeAluno { get; set; }
-        public int? LocalidadeId { get; set; }
-        public required string NomeLocalidade { get; set; }
-        public string? MunicipioEstado { get; set; }
-        public string? Sexo { get; set; }
-        public string? StatusLaudo { get; set; }
-        public DateTime? DtNascimento { get; set; }
-    }
+		public int Id { get; init; }
+
+		#region Ids
+
+		public int? TalentoEsportivoId { get; init; }
+		public int? VocacionalId { get; init; }
+		public int? QualidadeDeVidaId { get; init; }
+		public int? SaudeId { get; init; }
+		public int? ConsumoAlimentarId { get; init; }
+		public int? SaudeBucalId { get; init; }
+		public int? LocalidadeId { get; init; }
+		public int? AlunoId { get; init; }
+		public int? EncaminhamentoVocacionalId { get; init; }
+		public int? EncaminhamentoQualidadeVidaId { get; init; }
+		public int? EncaminhamentoConsumoAlimentarId { get; init; }
+		public int? EncaminhamentoSaudeBucalId { get; init; }
+		public int? EncaminhamentoTalentoEsportivoId { get; init; }
+
+		#endregion
+
+		#region Cabeçalho
+
+		public required string NomeAluno { get; init; }
+		public required string NomeLocalidade { get; init; }
+		public string? MunicipioEstado { get; init; }
+		public string? Sexo { get; init; }
+		public string? StatusLaudo { get; init; }
+		public DateTime? DtNascimento { get; init; }
+		public string? Email { get; init; }
+		public byte[]? QrCode { get; init; }
+		public decimal? Estatura { get; init; }
+		public decimal? Massa { get; init; }
+		public byte[]? ByteImage { get; init; }
+		public string? NomeFoto { get; init; }
+		public string? Modalidade { get; init; }
+		//public string? Serie { get; init; }
+		//public string? Turma { get; init; }
+		//public int? MunicipioId { get; init; }
+		//public string? NomeMunicipio { get; init; }
+
+		#endregion
+
+		#region Saude
+
+		public string? ImcSaude { get; init; }
+
+		#endregion
+	}
 }
