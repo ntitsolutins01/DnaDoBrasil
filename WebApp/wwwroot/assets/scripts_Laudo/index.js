9ca6b39282d1096cbf74f6092763c6ba25be0838
Fabio Muniz Developer <fabio.muniz01@gmail.com>
2024-10-16 16:32:30 -0300

    Revert "feat: details completo"
    This reverts commit de8513137d34c005d0157aef7c4e73b48b9aa51e.

--- WebApp/wwwroot/assets/scripts_Laudo/index.js
+++ WebApp/wwwroot/assets/scripts_Laudo/index.js
@@ -5,76 +5,63 @@ const nextButton = document.getElementById('nextButton');
 document.getElementById('totalPages').textContent = totalPages;
 
 function updateNavigationState() {
-    // Desabilita o botão "anterior" se estiver na primeira página
-    if (currentPageNumber <= 1) {
-        prevButton.classList.add('disabled');
-    } else {
-        prevButton.classList.remove('disabled');
-    }
+        // Desabilita o botão "anterior" se estiver na primeira página
+        if (currentPageNumber <= 1) {
+                prevButton.classList.add('disabled');
+        } else {
+                prevButton.classList.remove('disabled');
+        }
 
-    // Desabilita o botão "próximo" se estiver na última página
-    if (currentPageNumber >= totalPages) {
-        nextButton.classList.add('disabled');
-    } else {
-        nextButton.classList.remove('disabled');
-    }
+        // Desabilita o botão "próximo" se estiver na última página
+        if (currentPageNumber >= totalPages) {
+                nextButton.classList.add('disabled');
+        } else {
+                nextButton.classList.remove('disabled');
+        }
 
-    document.getElementById('currentPage').textContent = currentPageNumber;
+        document.getElementById('currentPage').textContent = currentPageNumber;
 }
 
 function previousPage() {
-    if (currentPageNumber > 1) {
-        currentPageNumber--;
-        const prevPage = document.querySelectorAll('.page-container')[currentPageNumber - 1];
-        prevPage.scrollIntoView({ behavior: 'smooth' });
-        //updateNavigationState();
-    }
+        if (currentPageNumber > 1) {
+                currentPageNumber--;
+                const prevPage = document.querySelectorAll('.page-container')[currentPageNumber - 1];
+                prevPage.scrollIntoView({ behavior: 'smooth' });
+                //updateNavigationState();
+        }
 }
 function nextPage() {
-    if (currentPageNumber < totalPages) {
-        currentPageNumber++;
-        const nextPage = document.querySelectorAll('.page-container')[currentPageNumber - 1];
-        nextPage.scrollIntoView({ behavior: 'smooth' });
-        //updateNavigationState();
-    }
+        if (currentPageNumber < totalPages) {
+                currentPageNumber++;
+                const nextPage = document.querySelectorAll('.page-container')[currentPageNumber - 1];
+                nextPage.scrollIntoView({ behavior: 'smooth' });
+                //updateNavigationState();
+        }
 }
 
 function printDocument() {
-    window.print();
+        window.print();
 }
 function downloadDocument() {
-    //document.getElementById("download")
-    //    .addEventListener("click", () => {
-    //        const invoice = this.document.getElementById("invoice");
-    //        console.log(invoice);
-    //        var opt = {
-    //            margin: 0,
-    //            filename: 'myfile.pdf',
-    //            image: { type: 'jpeg', quality: 1 },
-    //            html2canvas: { scale: 1 },
-    //            jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
-    //        };
-    //        html2pdf().from(invoice).set(opt).save();
-    //    })
-    window.print();
-}
-
-
+        console.log('Download iniciado');
+        window.print();
+    }
+    
 
 // Observador de scroll para atualizar o número da página
 const observerOptions = {
-    root: null,
-    threshold: 0.5,
+        root: null,
+        threshold: 0.5,
 };
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            const pages = document.querySelectorAll('.page-container');
-            currentPageNumber = Array.from(pages).indexOf(entry.target) + 1;
-            updateNavigationState();
-        }
-    });
+        entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                        const pages = document.querySelectorAll('.page-container');
+                        currentPageNumber = Array.from(pages).indexOf(entry.target) + 1;
+                        updateNavigationState();
+                }
+        });
 }, observerOptions);
 document.querySelectorAll('.page-container').forEach((page) => {
-    observer.observe(page);
+        observer.observe(page);
 });
