4e3254fd61fef5dc43535e83a1eaba0242924065
Fabio Muniz Developer <fabio.muniz01@gmail.com>
2024-12-19 09:52:47 -0300

    chore: implementação de indicadores front e combos para pesquisa

--- WebApp/Controllers/DashboardEadController.cs
+++ WebApp/Controllers/DashboardEadController.cs
@@ -1,8 +1,4 @@
-﻿using System.IdentityModel.Tokens.Jwt;
-using System.Net;
-using System.Security.Claims;
-using System.Security.Principal;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Options;
@@ -29,14 +25,55 @@ namespace WebApp.Controllers
         }
         public async Task<IActionResult> Index(IFormCollection collection)
         {
-            var usu = User?.Identity.Name;
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var userName = User.FindFirstValue(ClaimTypes.Name); // will give the user's userName
-            var email = User.FindFirstValue(ClaimTypes.Email);
-            var token = HttpContext.Request.Cookies["token"];
+            //var usu = User?.Identity.Name;
+            //var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            //var userName = User.FindFirstValue(ClaimTypes.Name); // will give the user's userName
+            //var email = User.FindFirstValue(ClaimTypes.Email);
+            //var token = HttpContext.Request.Cookies["token"];
             //ApplicationSettings.Token = HttpContext.Request.Cookies["token"];
 
-            return View();
+            var searchFilter = new DashboardEadDto
+            {
+                FomentoId = collection["ddlFomento"].ToString(),
+                Estado = collection["ddlEstado"].ToString(),
+                MunicipioId = collection["ddlMunicipio"].ToString(),
+                LocalidadeId = collection["ddlLocalidade"].ToString(),
+                DeficienciaId = collection["ddlDeficiencia"].ToString(),
+                Etnia = collection["ddlEtnia"].ToString()
+            };
+
+            var dashboardEad = new DashboardEadDto();
+
+            var fomentos = new SelectList(ApiClientFactory.Instance.GetFomentoAll(), "Id", "Nome", dashboardEad.FomentoId);
+            var estados = new SelectList(ApiClientFactory.Instance.GetEstadosAll(), "Sigla", "Nome", dashboardEad.Estado);
+            var tipoCurso = new SelectList(ApiClientFactory.Instance.GetTipoCursosAll(), "Id", "Nome");
+
+            SelectList municipios = null;
+
+            if (!string.IsNullOrEmpty(dashboardEad.Estado))
+            {
+                municipios = new SelectList(ApiClientFactory.Instance.GetMunicipiosByUf(dashboardEad.Estado), "Id", "Nome", dashboardEad.MunicipioId);
+            }
+            SelectList localidades = null;
+
+            if (!string.IsNullOrEmpty(dashboardEad.LocalidadeId))
+            {
+                localidades = new SelectList(ApiClientFactory.Instance.GetLocalidadeByMunicipio(dashboardEad.MunicipioId), "Id", "Nome", dashboardEad.LocalidadeId);
+            }
+
+            var model = new DashboardEadModel
+            {
+                ListFomentos = fomentos,
+                ListEstados = estados,
+                Dashboard = dashboardEad,
+                ListMunicipios = municipios!,
+                ListTipoCursos = tipoCurso,
+                ListLocalidades = localidades!
+            };
+
+
+
+            return View(model);
         }
     }
 }
---
+++ WebApp/Models/DashboardEadModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApp.Dto;
+
+namespace WebApp.Models
+{
+    public class DashboardEadModel
+    {
+        public string FomentoId { get; set; }
+        public SelectList ListFomentos { get; set; }
+        public string LocalidadeId { get; set; }
+        public SelectList ListLocalidades { get; set; }
+        public string EstadoId { get; set; }
+        public SelectList ListEstados { get; set; }
+        public string MunicipioId { get; set; }
+        public SelectList ListMunicipios { get; set; }
+        public string CursoId { get; set; }
+        public SelectList ListCursos { get; set; }
+        public string TipoCursosId { get; set; }
+        public SelectList ListTipoCursos { get; set; }
+        public DashboardEadDto Dashboard { get; set; }
+    }
+
+
+}
