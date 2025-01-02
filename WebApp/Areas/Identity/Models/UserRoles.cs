79de74a049d5752b0ca6c67d2263f5c187ebd7f8
Fabio Muniz Developer <fabio.muniz01@gmail.com>
2025-01-02 14:15:42 -0300

    chore: ajustes de nomenclaturas para gestor e coordenador

--- WebApp/Areas/Identity/Models/UserRoles.cs
+++ WebApp/Areas/Identity/Models/UserRoles.cs
@@ -7,9 +7,9 @@
         public const string Aluno = "Aluno";
         public const string Parceiro = "Parceiro";
         public const string Profissional = "Profissional";
-        public const string Gestor = "Gestor";
+        public const string Gestor = "Gestor Pedagógico";
         public const string ProfessorEad = "ProfessorEad";
         public const string CoordenadorEad = "CoordenadorEad";
-        public const string Coordenador = "Coordenador";
+        public const string Coordenador = "Coordenador  Pedagógico";
     }
 }
