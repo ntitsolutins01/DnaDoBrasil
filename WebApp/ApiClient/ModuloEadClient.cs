42dd869fff6b8748e77e8b3d6ec398f990bf6969
Nacelio.Code <nacelio.code@gmail.com>
2025-01-10 16:37:16 -0300

    chore: summary ModuloEadClient

--- WebApp/ApiClient/ModuloEadClient.cs
+++ WebApp/ApiClient/ModuloEadClient.cs
@@ -3,18 +3,33 @@ using WebApp.Models;
 
 namespace WebApp.ApiClient
 {
+    /// <summary>
+    /// Modulo Ead Client
+    /// </summary>
     public partial class DnaApiClient
     {
 	    private const string ResourceModuloEad = "ModulosEad";
 
-		#region Main Methods
+        #region Main Methods
 
-		public Task<long> CreateModuloEad (ModuloEadModel.CreateUpdateModuloEadCommand command)
+        /// <summary>
+        /// Inclusão de Modulo Ead
+        /// </summary>
+        /// <param name="command">Objeto de inclusão da Modulo Ead</param>
+        /// <returns>Id do Modulo Ead inserido</returns>
+        public Task<long> CreateModuloEad (ModuloEadModel.CreateUpdateModuloEadCommand command)
         {
             var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
                 $"{ResourceModuloEad}"));
             return Post(requestUrl, command);
         }
+
+        /// <summary>
+        /// Alteração de Modulo Ead
+        /// </summary>
+        /// <param name="id">Id de Alteração da Modulo Ead</param>
+        /// <param name="command">Objeto de alteração da Modulo Ead</param>
+        /// <returns>Retorna true ou false</returns>
         public Task<bool> UpdateModuloEad (int id, ModuloEadModel.CreateUpdateModuloEadCommand command)
         {
             var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
@@ -22,6 +37,11 @@ namespace WebApp.ApiClient
             return Put(requestUrl, command);
         }
 
+        /// <summary>
+        /// Exclusão de Modulo Ead
+        /// </summary>
+        /// <param name="id">Id de exclusao da Modulo Ead</param>
+        /// <returns>Retorna true ou false</returns>
         public Task<bool> DeleteModuloEad (int id)
         {
             var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
@@ -33,18 +53,34 @@ namespace WebApp.ApiClient
 
         #region Methods
 
+        /// <summary>
+        ///  Busca um único Modulo Ead
+        /// </summary>
+        /// <param name="id">Id da Modulo Ead a ser buscada</param>
+        /// <returns>Retorna o objeto da Modulo Ead</returns>
         public ModuloEadDto GetModuloEadById(int id)
         {
             var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
                 $"{ResourceModuloEad}/{id}"));
             return Get<ModuloEadDto>(requestUrl);
         }
+
+        /// <summary>
+        /// Busca todas as Modulos Ead cadastradas
+        /// </summary>
+        /// <returns>Retorna a lista de Modulos Ead</returns>
         public List<ModuloEadDto> GetModulosEadAll()
         {
             var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
                 $"{ResourceModuloEad}"));
             return Get<List<ModuloEadDto>>(requestUrl);
         }
+
+        /// <summary>
+        /// Busca todos os modulo Ead por Curso id
+        /// </summary>
+        /// <param name="cursoId">id por Curso</param>
+        /// <returns>retorna lista de Modulo Ead por Curso</returns>
         public List<ModuloEadDto> GetModulosEadAllByCursoId(int cursoId)
         {
             var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
