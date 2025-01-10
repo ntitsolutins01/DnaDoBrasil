71019e4c32dd9a2df69cdf776ad611475ac3d9c9
Nacelio.Code <nacelio.code@gmail.com>
2025-01-10 12:00:20 -0300

    chore: summary DivisaoAdministrativaClient

--- WebApp/ApiClient/DivisaoAdministrativaClient.cs
+++ WebApp/ApiClient/DivisaoAdministrativaClient.cs
@@ -4,24 +4,43 @@ using WebApp.Models;
 
 namespace WebApp.ApiClient
 {
+    /// <summary>
+    /// Divisao Administrativa Client
+    /// </summary>
     public partial class DnaApiClient
 	{
 		private const string ResourceDivisaoAdministrativa = "DivisoesAdministrativas";
 
-		#region Methods
+        #region Methods
 
-		public MunicipioDto GetMunicipiosByid(string id)
+        /// <summary>
+        /// Busca todos os Municipios por Id
+        /// </summary>
+        /// <param name="id">Id de Municipio a ser buscado</param>
+        /// <returns>Retorna o objeto de Municipio</returns>
+        public MunicipioDto GetMunicipiosByid(string id)
         {
             var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
                 $"{ResourceDivisaoAdministrativa}/{id}"));
             return Get<MunicipioDto>(requestUrl);
         }
+
+        /// <summary>
+        /// Busca todos os Estados
+        /// </summary>
+        /// <returns>Retorna a lisa de Estados</returns>
         public List<EstadoDto> GetEstadosAll()
         {
             var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
                 $"{ResourceDivisaoAdministrativa}/Estados"));
             return Get<List<EstadoDto>>(requestUrl);
         }
+
+        /// <summary>
+        ///  Busca todos os Municipios pela Uf
+        /// </summary>
+        /// <param name="uf">uf</param>
+        /// <returns>Retorna um objeto de Municipio pela Uf</returns>
         public List<MunicipioDto> GetMunicipiosByUf(string uf)
         {
 			var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
