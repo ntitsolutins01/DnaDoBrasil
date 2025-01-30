bff3fddf48b3fc514824c86153b86c255a5a88b6
Nacelio.Code <nacelio.code@gmail.com>
2025-01-30 08:34:48 -0300

    chore: refatoracao da controler e summary

--- WebApp/Controllers/ContratoController.cs
+++ WebApp/Controllers/ContratoController.cs
@@ -1,9 +1,6 @@
-﻿using Infraero.Relprev.CrossCutting.Enumerators;
-using Microsoft.AspNetCore.Identity;
+﻿using DocumentFormat.OpenXml.Spreadsheet;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Options;
-using WebApp.Areas.Identity.Models;
 using WebApp.Configuration;
 using WebApp.Dto;
 using WebApp.Enumerators;
@@ -13,52 +10,87 @@ using WebApp.Utility;
 
 namespace WebApp.Controllers
 {
+	/// <summary>
+	/// Controle de Contrato
+	/// </summary>
     public class ContratoController : BaseController
 	{
-		private readonly IOptions<UrlSettings> _appSettings;
+        #region Parametros
 
-		public ContratoController(IOptions<UrlSettings> appSettings)
-		{
-			_appSettings = appSettings;
-			ApplicationSettings.WebApiUrl = _appSettings.Value.WebApiBaseUrl;
-		}
+        private readonly IOptions<UrlSettings> _appSettings;
 
-		public IActionResult Index(int? crud, int? notify, string message = null)
-		{
-			SetNotifyMessage(notify, message);
-			SetCrudMessage(crud);
-			var response = ApiClientFactory.Instance.GetContratoAll();
+        #endregion
 
-			return View(new ContratoModel() { Contratos = response });
-		}
+        #region Constructor
 
-		//[ClaimsAuthorize("ConfiguracaoSistema", "Incluir")]
-		public ActionResult Create(int? crud, int? notify, string message = null)
-		{
-			SetNotifyMessage(notify, message);
-			SetCrudMessage(crud);
+        /// <summary>
+        /// Construtor da página
+        /// </summary>
+        /// <param name="appSettings">configurações de urls do sistema</param>
+        public ContratoController(IOptions<UrlSettings> appSettings)
+        {
+            _appSettings = appSettings;
+            ApplicationSettings.WebApiUrl = _appSettings.Value.WebApiBaseUrl;
+        }
+        #endregion
 
-			return View();
-		}
+        #region Main Methods
 
-		//[ClaimsAuthorize("Usuario", "Incluir")]
-		[HttpPost]
-		public async Task<ActionResult> Create(IFormCollection collection)
-		{
-			try
-			{
-				var command = new ContratoModel.CreateUpdateContratoCommand
-				{
-					Nome = collection["nome"].ToString().ToUpper(),
-					Descricao = collection["descricao"].ToString(),
-					DtIni = collection["dtini"].ToString(),
-					DtFim = collection["dtfim"].ToString(),
-					Anexo = collection["anexo"].ToString()
-				};
+        /// <summary>
+        /// Listagem de Contrato
+        /// </summary>
+        /// <param name="crud">paramentro que indica o tipo de ação realizado</param>
+        /// <param name="notify">parametro que indica o tipo de notificação realizada</param>
+        /// <param name="message">mensagem apresentada nas notificações e alertas gerados na tela</param>
+        /// <returns>returns true false</returns>
+        public IActionResult Index(int? crud, int? notify, string message = null)
+        {
+            SetNotifyMessage(notify, message);
+            SetCrudMessage(crud);
+            var response = ApiClientFactory.Instance.GetContratoAll();
 
-				await ApiClientFactory.Instance.CreateContrato(command);
+            return View(new ContratoModel() { Contratos = response });
+        }
 
-				return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Created });
+        /// <summary>
+        /// Tela para Inclusão de Contrato
+        /// </summary>
+        /// <param name="crud">paramentro que indica o tipo de ação realizado</param>
+        /// <param name="notify">parametro que indica o tipo de notificação realizada</param>
+        /// <param name="message">mensagem apresentada nas notificações e alertas gerados na tela</param>
+        /// <returns>returns true false</returns>
+        //[ClaimsAuthorize("ConfiguracaoSistema", "Incluir")]
+        public ActionResult Create(int? crud, int? notify, string message = null)
+        {
+            SetNotifyMessage(notify, message);
+            SetCrudMessage(crud);
+
+            return View();
+        }
+
+        /// <summary>
+        /// Ação de Inclusão de Contrato
+        /// </summary>
+        /// <param name="collection">Coleção de dados para inclusao de Contrato</param>
+        /// <returns>retorna mensagem de inclusao através do parametro crud</returns>
+        //[ClaimsAuthorize("Usuario", "Incluir")]
+        [HttpPost]
+        public async Task<ActionResult> Create(IFormCollection collection)
+        {
+            try
+            {
+                var command = new ContratoModel.CreateUpdateContratoCommand
+                {
+                    Nome = collection["nome"].ToString().ToUpper(),
+                    Descricao = collection["descricao"].ToString(),
+                    DtIni = collection["dtini"].ToString(),
+                    DtFim = collection["dtfim"].ToString(),
+                    Anexo = collection["anexo"].ToString()
+                };
+
+                await ApiClientFactory.Instance.CreateContrato(command);
+
+                return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Created });
             }
             catch (Exception e)
             {
@@ -68,45 +100,77 @@ namespace WebApp.Controllers
             }
         }
 
-		//[ClaimsAuthorize("Usuario", "Alterar")]
-		public async Task<ActionResult> Edit(IFormCollection collection)
-		{
-			var command = new ContratoModel.CreateUpdateContratoCommand
-			{
-				Id = Convert.ToInt32(collection["editContratoId"]),
-				Nome = collection["nome"].ToString().ToUpper(),
-				Descricao = collection["descricao"].ToString(),
-				DtIni = collection["dtini"].ToString(),
-				DtFim = collection["dtfim"].ToString(),
-				Status = collection["editStatus"].ToString() == "" ? false : true,
-				Anexo = collection["anexo"].ToString()
-			};
+        /// <summary>
+        ///  Ação de Alteração de Contrato
+        /// </summary>
+        /// <param name="collection">coleção de dados para alteração de Categoria</param>
+        /// <returns>retorna mensagem de alteração através do parametro crud</returns>
+        //[ClaimsAuthorize("Usuario", "Alterar")]
+        public async Task<ActionResult> Edit(IFormCollection collection)
+        {
+            var command = new ContratoModel.CreateUpdateContratoCommand
+            {
+                Id = Convert.ToInt32(collection["editContratoId"]),
+                Nome = collection["nome"].ToString().ToUpper(),
+                Descricao = collection["descricao"].ToString(),
+                DtIni = collection["dtini"].ToString(),
+                DtFim = collection["dtfim"].ToString(),
+                Status = collection["editStatus"].ToString() == "" ? false : true,
+                Anexo = collection["anexo"].ToString()
+            };
 
-			await ApiClientFactory.Instance.UpdateContrato(command.Id, command);
+            await ApiClientFactory.Instance.UpdateContrato(command.Id, command);
 
-			return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Updated });
-		}
+            return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Updated });
+        }
 
-		//[ClaimsAuthorize("Usuario", "Excluir")]
-		public ActionResult Delete(int id)
-		{
-			try
-			{
-				ApiClientFactory.Instance.DeleteContrato(id);
-				return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Deleted });
-			}
-			catch
-			{
-				return RedirectToAction(nameof(Index));
-			}
-		}
+        /// <summary>
+        /// Ação de Exclusão de Contrato
+        /// </summary>
+        /// <param name="id">identificador do Contrato</param>
+        /// <returns>retorna mensagem de exclusão através do parametro crud</returns>
+        //[ClaimsAuthorize("Usuario", "Excluir")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                ApiClientFactory.Instance.DeleteContrato(id);
+                return RedirectToAction(nameof(Index), new { crud = (int)EnumCrud.Deleted });
+            }
+            catch
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
 
-		public Task<ContratoDto> GetContratoById(int id)
-		{
-			var result = ApiClientFactory.Instance.GetContratoById(id);
+        #endregion
 
-			return Task.FromResult(result);
-		}
-	}
+
+        #region Get Methods
+
+        /// <summary>
+        ///  Busca Contrato por Id
+        /// </summary>
+        /// <param name="id">Identificador de Contrato</param>
+        /// <returns>Retorna a Contrato</returns>
+        public Task<ContratoDto> GetContratoById(int id)
+        {
+            var result = ApiClientFactory.Instance.GetContratoById(id);
+
+            return Task.FromResult(result);
+        }
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+    }
 }
 
